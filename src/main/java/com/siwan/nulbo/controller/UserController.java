b22651b87dedcbd3bbf4d731bfe6b9c7dd922140
jooyh <aguitarj@naver.com>
2020-02-15 12:51:38 +0900

    -

--- src/main/java/com/siwan/nulbo/controller/UserController.java
+++ src/main/java/com/siwan/nulbo/controller/UserController.java
@@ -1,8 +1,20 @@
 package com.siwan.nulbo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class UserController {
-
+@RequestMapping("/user")
+public class UserController extends BaseController{
+	
+	public Map getUserInfo(HttpServletRequest request) {
+		Map rtnMap = new HashMap();
+		Map paramMap = getParamMap(request);
+		return rtnMap;
+	}
 }
