466ceecd22347afa08bd497753ee3f2906e3f08e
jooyh <aguitarj@naver.com>
2020-02-15 13:32:00 +0900

    xml To JAVA

---
+++ src/main/java/configuration/ServletConfiguration.java
@@ -0,0 +1,47 @@
+package configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.siwan.nulbo.utils.Interceptor;
+
+
+@Configuration 
+@EnableWebMvc 
+@EnableAsync // @Async 어노테이션을 사용하기 위함 
+@ComponentScan(basePackages="com.siwan.nulbo", excludeFilters=@ComponentScan.Filter(Configuration.class)) 
+public class ServletConfiguration extends WebMvcConfigurerAdapter{
+ 
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    /** * 인터셉터 추가 */ 
+    @Override 
+    public void addInterceptors(InterceptorRegistry registry) { 
+    	registry.addInterceptor(new Interceptor()); 
+    } 
+    
+}
+
