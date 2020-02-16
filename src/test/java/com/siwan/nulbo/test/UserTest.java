3eb902c94e56530d371adc4a791176210fda266e
jooyh <aguitarj@naver.com>
2020-02-16 21:29:35 +0900

    JAVA VERSION CHANGE

--- src/test/java/com/siwan/nulbo/test/UserTest.java
+++ src/test/java/com/siwan/nulbo/test/UserTest.java
@@ -1,31 +1,21 @@
 package com.siwan.nulbo.test;
 
-import org.apache.ibatis.session.SqlSession;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.siwan.nulbo.controller.BaseController;
 import com.siwan.nulbo.service.UserService;
 
 import configuration.RootConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes={RootConfiguration.class, UserService.class})
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes={RootConfiguration.class, UserService.class})
 public class UserTest {
 
-	protected static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+	protected static final Logger logger = LoggerFactory.getLogger(UserTest.class);
 	
-	@Autowired
-	UserService userService;
-	
-	@Test
-	public void selectUser() {
-		System.out.println(userService.selectUser().toString());
-	}
+
 	
 }
